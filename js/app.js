ba0943f8acf6b267ed0d36069931f152dc2274d8
Arthur LEROUX <arthur.leroux33@protonmail.com>
2023-07-11 23:03:04 +0200

    search cellElement for the classList add targetCell

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const app = {
   x: 0,
   y: 0,
   direction: "right",
-  targetCell: {
+  targetCell : {
     x: 5,
     y: 3,
   },
@@ -12,20 +12,27 @@ const app = {
     const boardElement = document.querySelector(`#board`);
     for (let i = 0; i < 4; i++) {
       //première boucle pour les div row adopter par #board
-      
+
       const rowElement = document.createElement(`div`);
       rowElement.classList.add(`row`);
       boardElement.appendChild(rowElement);
-      console.log(rowElement);
-    
-    for (let j = 0; j < 6; j++) {
-      //deuxième boucle pour les div cell adopter par les div row
-      const cellElement = document.createElement(`div`);
-      cellElement.classList.add(`cell`);
-      rowElement.appendChild(cellElement);
-      console.log(cellElement);
+      //console.log(rowElement);
+
+      for (let j = 0; j < 6; j++) {
+        //deuxième boucle pour les div cell adopter par les div row
+        const cellElement = document.createElement(`div`);
+        cellElement.classList.add(`cell`);
+        rowElement.appendChild(cellElement);
+        // console.log(cellElement);
+      }
     }
-  }
+    const cellElements = document.querySelectorAll(`.cell`);
+    
+    console.log(cellElements);
+    cellElements.forEach((cellElement) => {
+      const cell = [cellElement[23]]
+     console.log(cell);
+    });
   },
 
   init: function () {
@@ -33,5 +40,6 @@ const app = {
     app.drawBoard(); // initialisation de la fonction d création de la grille
   },
 };
+console.log(app.targetCell);
 
 document.addEventListener("DOMContentLoaded", app.init);
