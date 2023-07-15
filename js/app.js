7a927d0838f4e99e1b215fef69d0db75e32a2a72
Arthur LEROUX <arthur.leroux33@protonmail.com>
2023-07-15 22:41:23 +0200

    feat : switch direction for the player

--- js/app.js
+++ js/app.js
@@ -34,8 +34,7 @@ const app = {
           playerDiv.classList.add("player");
           cellElement.appendChild(playerDiv);
 
-          
-          playerDiv.classList.add(app.player.direction); 
+          playerDiv.classList.add(app.player.direction);
         }
         if (app.targetCell.y === rowIndex && app.targetCell.x === cellIndex) {
           cellElement.classList.add(`target-cell`);
@@ -43,32 +42,46 @@ const app = {
       }
     }
   },
-    //On vide le contenu de la grille "#board"
+  //On vide le contenu de la grille "#board"
   clearBoard: function () {
     const boardElement = document.querySelector(`#board`);
     boardElement.innerHTML = "";
   },
-  redrawBoard: function (){
+  redrawBoard: function () {
     app.drawBoard();
     app.clearBoard();
   },
-   turnRight : function (){
-      // Si le player est orienté vers le up => right
-      // Si le player est orienté vers le right => down
-      // Si le player est orienté vers le down => left
-      // Si le player est orienté vers le left => up
-   },
+  turnRight: function () {
+    // Si le player est orienté vers le up => right
+    // Si le player est orienté vers le right => down
+    // Si le player est orienté vers le down => left
+    // Si le player est orienté vers le left => up
+    // sémentiquement la même choses plus de 3 fois => utiliser un switch
+    switch (player.direction) {
+      case "up": //=> Si la case player vaut up
+        player.direction = "right"; //=>  je rotate vers right
+        break; //=> je pas oublier le break.
+      case "right":
+        player.direction = "down";
+        break;
+      case "down":
+        player.direction = "left";
+        break;
+      case "left":
+        player.direction = "up";
+        break;
+      default:
+        console.log(`There is an error in the player position`);
+    }
+  },
 
   init: function () {
     console.log("init !");
     app.drawBoard(); // initialisation de la fonction d création de la grille
     //app.clearBoard();
-     //app.redrawBoard();
+    //app.redrawBoard();
   },
 };
 console.log(app.targetCell);
 
-
 document.addEventListener("DOMContentLoaded", app.init);
-
-
